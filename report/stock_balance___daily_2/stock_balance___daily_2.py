d1e8cacc09ebea198ddfeb3f7d8ce4f06adf0fea
Dev2-dipanetech <118428224+Dev2-dipanetech@users.noreply.github.com>
2022-12-30 12:54:19 +0530

    multiple days changes in the inventory

--- report/stock_balance___daily_2/stock_balance___daily_2.py
+++ report/stock_balance___daily_2/stock_balance___daily_2.py
@@ -9,6 +9,8 @@ def execute(filters=None):
 	data = get_data()
 	return columns, data
 
+d_data = frappe.get_all(doctype = "Stock Ledger Entry",fields = ['item_code', 'warehouse','posting_date','voucher_type','company','actual_qty'], order_by = 'posting_date asc')
+
 def get_columns():
 	columns = [
 		{
@@ -33,16 +35,54 @@ def get_columns():
 		},
         
 	]
+	# for date in range(1,31):
+	# 	col_date = {
+	# 		"label":("Day_"+ str(date)) ,
+	# 		"fieldname":("day_"+ str(date)),
+	# 		"fieldtype": "Data",
+	# 		"width" : 150,
+	# 	}
+	# 	columns.append(col_date)
 
-	for date in range(1,31):
+	for d in d_data:
 		col_date = {
-			"label":("Day_"+ date) ,
-			"fieldname":("day_"+ date),
+			"label" : str(d.posting_date),
+			"fieldname": str(d.posting_date),
 			"fieldtype": "Data",
 			"width" : 150,
 		}
-		columns.append(col_date)
+		for c in columns:
+			flag = 0
+			if c["label"] == col_date["label"]:
+				flag = 1
+		if flag ==  0:
+			columns.append(col_date)
+	
+
+
 	return columns
 
 def get_data():
 	
+	data =[]
+	
+	for d in d_data:
+		# day = str(int((str(d.posting_date))[8:]))
+		row = {
+			'item_code': d.item_code,
+			'company': d.company,
+			'warehouse': d.warehouse,
+			# ('day_'+ day): d.actual_qty,
+			str(d.posting_date): d.actual_qty
+		}
+		flag = 1
+		for dic in data:       
+			if (row["item_code"] == dic["item_code"]) & (row["warehouse"] == dic["warehouse"]) & (row["company"] == dic["company"]):
+				flag = 0
+				if str(d.posting_date) not in dic.keys():
+					dic[str(d.posting_date)] = row[str(d.posting_date)]
+				else:
+					dic[str(d.posting_date)] += row[str(d.posting_date)]
+		if flag == 1:
+			data.append(row)
+	return data
